b860af1f414fac1277a5f3bd7c2324a7b5cde04a
Max Rukomoynikov <max@apply4.com>
2022-07-02 13:24:27 +0400

    Github Client

--- lib/act_as_api_client/clients/github_client.rb
+++ lib/act_as_api_client/clients/github_client.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
 require_relative "http_client"
-require 'httparty'
+require "httparty"
 
 module ActAsApiClient
   module Clients
     module GithubClient
       include HttpClient
-      def find
-        HTTParty.get('https://api.github.com/repositories', headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/vnd.github.v3+json' }).parsed_response
+      def find(repository_name)
+        unless repository_name.match?(%r{[a-zA-Z]/[a-zA-Z]})
+          raise StandardError, "repository_name parameter is not valid"
+        end
 
-        # get(, {
-        #
-        # })
+        HTTParty
+          .get(
+            "https://api.github.com/repos/#{repository_name}",
+            headers: { "Content-Type" => "application/json", "Accept" => "application/vnd.github.v3+json" }
+          )
+          .parsed_response
       end
 
       def where
--- spec/clients/github_client_spec.rb
+++ spec/clients/github_client_spec.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-require 'act_as_api_client/clients/github_client'
-require 'act_as_api_client'
+require "act_as_api_client/clients/github_client"
+require "act_as_api_client"
 
 RSpec.describe ActAsApiClient::Clients::GithubClient do
-  let(:github_client) {
+  let(:github_client) do
     class GithubClient < ApiClient
       act_as_api_client for: :github,
                         with: {
-                          token: YAML.load_file('spec/credentials.yml').dig('github', 'token')
+                          token: YAML.load_file("spec/credentials.yml").dig("github", "token")
                         }
     end
 
     GithubClient.new
-  }
+  end
 
-  context 'when correct options are provided', :vcr do
-    it 'returns list of repositories (#find)' do
-      expect(github_client.find).to be_a_kind_of(Array)
+  context "when correct options are provided", :vcr do
+    it "returns list of repositories (#find)" do
+      expect(github_client.find('Rukomoynikov/tabled')).to include({"full_name"=>"Rukomoynikov/tabled"})
     end
   end
 end
