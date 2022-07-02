7c47c5bb18711e73876d9deb652739bc395e6846
Max Rukomoynikov <max@apply4.com>
2022-07-02 13:04:37 +0400

    Basic implementation of Github Client

--- lib/act_as_api_client/clients/github_client.rb
+++ lib/act_as_api_client/clients/github_client.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "http_client"
+require 'httparty'
 
 module ActAsApiClient
   module Clients
     module GithubClient
       include HttpClient
       def find
-        get('https://api.github.com/repositories', {
-          headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
-        })
+        HTTParty.get('https://api.github.com/repositories', headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/vnd.github.v3+json' }).parsed_response
+
+        # get(, {
+        #
+        # })
       end
 
       def where
