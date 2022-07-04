aa0b3d727ad0596d4c760f5f146c22b61f46b941
Max Rukomoynikov <max@apply4.com>
2022-07-04 17:23:02 +0400

    Using credentials for CI

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -6,4 +6,6 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
+
+  config.filter_sensitive_data("<GITHUB_TOKEN>") { YAML.load_file("spec/credentials.yml").dig("github", "token") }
 end
