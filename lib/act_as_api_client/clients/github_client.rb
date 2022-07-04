6c3bf4bd75f329ef165c16cf2d85fa1681592133
Max Rukomoynikov <max@apply4.com>
2022-07-04 17:23:02 +0400

    Using credentials for CI

--- lib/act_as_api_client/clients/github_client.rb
+++ lib/act_as_api_client/clients/github_client.rb
@@ -15,7 +15,9 @@ module ActAsApiClient
         HTTParty
           .get(
             "https://api.github.com/repos/#{repository_name}",
-            headers: { "Content-Type" => "application/json", "Accept" => "application/vnd.github.v3+json" }
+            headers: { "Content-Type" => "application/json",
+                       "Accept" => "application/vnd.github.v3+json",
+                       "Authorization" => (options[:token] ? "token #{options[:token]}" : nil) }
           )
           .parsed_response
       end
@@ -24,11 +26,15 @@ module ActAsApiClient
         "where"
       end
 
-      def find_by
+      def find_by(options = {})
+        # by_organization https://docs.github.com/en/rest/repos/repos#list-organization-repositories
+        # by_user https://docs.github.com/en/rest/repos/repos#list-repositories-for-a-user
+        # by_authenticated_user https://docs.github.com/en/rest/repos/repos#list-repositories-for-the-authenticated-user
         "find_by"
       end
 
       def delete
+        # Call only on queried before repository and repository is not 404/400 and has right to delete (write)
         "delete"
       end
 
