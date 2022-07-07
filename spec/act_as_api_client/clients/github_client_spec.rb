1ff8da8169960fe99e9bfc73f0c983532a61995e
Max Rukomoynikov <max@apply4.com>
2022-07-07 12:36:41 +0400

    Github client

--- spec/act_as_api_client/clients/github_client_spec.rb
+++ spec/act_as_api_client/clients/github_client_spec.rb
@@ -12,9 +12,21 @@ RSpec.describe ActAsApiClient::Clients::GithubClient do
     end
   end
 
-  context "when correct options are provided", :vcr do
-    it "returns list of repositories (#find)" do
-      expect(github_client_class.new.find("Rukomoynikov/tabled")).to include({ "full_name" => "Rukomoynikov/tabled" })
+  describe "#find", vcr: true do
+    context "when options are invalid" do
+      it "raises an exception" do
+        expect do
+          github_client_class.new.find("Rukomoynikovtabled")
+        end.to raise_error(StandardError, "repository_name parameter is not valid")
+      end
+    end
+
+    context "when options invalid" do
+      it "returns list of repositories (#find)" do
+        expect(
+          github_client_class.new.find("Rukomoynikov/tabled")
+        ).to include({ "full_name" => "Rukomoynikov/tabled" })
+      end
     end
   end
 end
