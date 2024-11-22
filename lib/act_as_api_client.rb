ded06eb53b7422421a3bce890ffd8a6e0eaa536c
Max Rukomoynikov <rukomoynikov@gmail.com>
2024-11-22 16:48:52 +0000

    Remove BaseApiMethods as ActiveRecord for Http clients didn't work out

--- lib/act_as_api_client.rb
+++ lib/act_as_api_client.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "act_as_api_client/base_api_methods"
 require "act_as_api_client/errors/non_existing_client_error"
 require "act_as_api_client/client_loader"
 
 class ApiClient
-  include ActAsApiClient::BaseApiMethods
-
   class << self
     def act_as_api_client(**args)
       set_general_client(client_for: args.fetch(:for, nil))
