91c60be3138f782d7a852bf3dc0afa1e9005dd80
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-11-07 17:32:37 +0000

    Include spec/credentials.yaml
    Remove github client from simple_client_spec

--- spec/act_as_api_client/clients/http/simple_client_spec.rb
+++ spec/act_as_api_client/clients/http/simple_client_spec.rb
@@ -3,13 +3,4 @@
 require "act_as_api_client/clients/http/simple_client"
 
 RSpec.describe ActAsApiClient::Clients::Http::SimpleClient do
-  let(:github_client_class) do
-    Class.new(ApiClient) do
-      act_as_api_client for: :github_repositories, with: { token: "token1" }
-    end
-  end
-
-  it "calls method from inherited class option 'for' is provided", :vcr do
-    expect { github_client_class.new.find("Rukomoynikov/tabled") }.not_to raise_error
-  end
 end
