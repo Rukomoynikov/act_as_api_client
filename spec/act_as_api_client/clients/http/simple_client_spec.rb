2939c978499df4d8f1116ca26ae8c21e28350563
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-11-07 18:14:43 +0000

    Process network errors correctly

--- spec/act_as_api_client/clients/http/simple_client_spec.rb
+++ spec/act_as_api_client/clients/http/simple_client_spec.rb
@@ -3,4 +3,15 @@
 require "act_as_api_client/clients/http/simple_client"
 
 RSpec.describe ActAsApiClient::Clients::Http::SimpleClient do
+  let(:simple_client) { subject }
+
+  context "when network is not available", vcr: { allow_unused_http_interactions: true } do
+    it "returns correct error" do
+      %i[get post].each do |method|
+        expect do
+          simple_client.send(method, "https://thisurldoesnexisthahaha")
+        end.to raise_error(ActAsApiClient::Errors::NetworkError)
+      end
+    end
+  end
 end
